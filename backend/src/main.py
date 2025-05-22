13d3319b11e72317c0e390ad610fcc008f948a21
maxtyrchan <matyrchan@googlemail.com>
2025-05-22 10:48:03 +0200

    installed langflow

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,26 +1,21 @@
-from unstructured.partition.pdf import partition_pdf
 import pytest
-from services.categorizer import categorize
+from langfuse import Langfuse
+import os
+from dotenv import load_dotenv
 
-file_path = "src/assets/Table.pdf"
+load_dotenv()
 
-# Partition the PDF file into elements via ocr
-#The hi_res_model_name parameter supports the yolox and detectron2_onnx arguments
-pdf_elements = partition_pdf(filename=file_path,
-                             strategy='hi_res',
-                             infer_table_structure=True,
-                             hi_res_model_name='yolox',
-                             extract_image_block_types=['Image'],
-                             #extract_image_block_to_payload=True, #If True, will extract base64 for API usage
-                             chunking_strategy='by_title',          # splitting strategy for the document (related elements are now grouped together)
-                             max_characters=10000,                  # defaults to 500
-                             combine_text_under_n_chars=2000,       # defaults to 0
-                             new_after_n_chars=6000,)
+azure_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+langfuse_secret_key = os.getenv("LANGFUSE_SECRET_KEY")
+langfuse_public_key = os.getenv("LANGFUSE_PUBLIC_KEY")
+langfuse_host = "http://localhost:3000"
 
-
-# Create a dictionary of the unique elements
-print(categorize(pdf_elements))
-
+langfuse = Langfuse(
+    secret_key=langfuse_secret_key,
+    public_key=langfuse_public_key,
+    host=langfuse_host
+)
 # Next we can focus on handling the different types of elements such as text, image, table, etc.
 
 # if __name__ == '__main__':
